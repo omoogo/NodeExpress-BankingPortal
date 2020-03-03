ca14dcc0fba7084418cafe676a20c8865729451a
Michael Broniola <mbroniola@gmail.com>
2020-03-02 21:01:16 -0800

    Updates for tasks 1-5 in the third module

--- src/app.js
+++ src/app.js
@@ -9,8 +9,18 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, './public')));
 
+const accountData = fs.readFileSync(path.join(__dirname, './json/accounts.json'), 'UTF8');
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync(path.join(__dirname, './json/users.json'), 'UTF8');
+const users = JSON.parse(userData);
+
 app.get('/', function(req, res) {
-    res.render('index', {title: 'Index'});
+    res.render('index', {title: 'Account Summary', accounts: accounts});
+});
+
+app.get('/savings', function(req, res) {
+    res.render('account', {account: accounts.savings});
 });
 
 app.listen(3000, () => {
