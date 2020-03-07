5dfa648f6195df26c692f093e574777aab8c7ff3
Michael Broniola <mbroniola@gmail.com>
2020-03-07 12:28:08 -0800

    Module 4 first task
    Required body parser middleware with urlencoded and extended set to true

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -8,6 +9,7 @@ app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, './public')));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const accountData = fs.readFileSync(path.join(__dirname, './json/accounts.json'), 'UTF8');
 const accounts = JSON.parse(accountData);
