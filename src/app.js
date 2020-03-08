89acd8322e979bd78c1f191f52344ad5bdf8e08d
Michael Broniola <mbroniola@gmail.com>
2020-03-08 14:12:37 -0700

    Module 4 transfer and payments feature

--- src/app.js
+++ src/app.js
@@ -37,6 +37,36 @@ app.get('/profile', function(req, res) {
     res.render('profile', {user: users[0]});
 });
 
+app.get('/transfer', function(req, res) {
+    res.render('transfer');
+});
+
+app.post('/transfer', function(req, res, next) {
+    // Set the from account new balance
+    accounts[req.body.from].balance -= req.body.amount;
+
+    // Set the to account new balance
+    accounts[req.body.to].balance += parseInt(req.body.amount, 10);
+
+    // Update account data
+    const accountsJSON = JSON.stringify(accounts);
+    fs.writeFileSync(path.join(__dirname, './json/accounts.json'), accountsJSON, 'UTF8');
+
+    res.render('transfer', { message: 'Transfer Completed'});
+});
+
+app.get('/payment', function(req, res) {
+    res.render('payment', { account: accounts.credit });
+});
+
+app.post('/payment', function(req, res) {
+    accounts.credit.balance -= req.body.amount;
+    accounts.credit.available += parseInt(req.body.amount, 10);
+    const accountsJSON = JSON.stringify(accounts);
+    fs.writeFileSync(path.join(__dirname, './json/accounts.json'), accountsJSON, 'UTF8');
+    res.render('payment', { message: 'Payment Successful', account: accounts.credit });
+});
+
 app.listen(3000, () => {
     console.log('PS Project Running on port 3000!');
 });
